215ab875c16ed208e14c543688a72aac8570a820
kablt--6362 <wogus6362@gmail.com>
2025-12-15 18:16:26 +0900

    test

--- backend/src/main/java/com/paynalty/global/response/ApiResponse.java
+++ backend/src/main/java/com/paynalty/global/response/ApiResponse.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ApiResponse<T> {
     private final boolean success;
+    private final T data;
+    private final String message;
 
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(true, data, null);
