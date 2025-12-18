bdb298b58402ae3343a1a6f8e065ae815d32b0f9
kablt--6362 <wogus6362@gmail.com>
2025-12-18 17:14:40 +0900

    refactor : 응답방식 변경(ApiResponse 한번 더 감싸기) Challenge 관계 설정: 불필요한 속성 제거(orphanRemoval)

--- backend/src/main/java/com/paynalty/domain/challenge/ChallengeController.java
+++ backend/src/main/java/com/paynalty/domain/challenge/ChallengeController.java
@@ -1,10 +1,10 @@
 package com.paynalty.domain.challenge;
 
+import com.paynalty.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -23,23 +23,23 @@ public class ChallengeController {
     private final ChallengeService challengeService;
 
     @PostMapping
-    public ResponseEntity<ChallengeResponse> createChallenge(
+    public ResponseEntity<ApiResponse<ChallengeResponse>> createChallenge(
             @Parameter(description = "챌린지 생성 요청 정보", required = true)
             @Valid @RequestBody ChallengeRequest request) {
         ChallengeResponse response = challengeService.create(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
 
     // 사용자가 참여중인 챌린지 중 챌린지 상태(인증,미인증)에 따른 챌린지 목록 요청
     @GetMapping("/{userId}/{status}")
-    public ResponseEntity<List<ChallengeResponse>> findByStatus(
+    public ResponseEntity<ApiResponse<List<ChallengeResponse>>> findByStatus(
             @Parameter(description = "사용자 ID", required = true, example = "1")
             @PathVariable Long userId,
             @Parameter(description = "챌린지 상태", required = true, example = "진행중")
             @PathVariable String status
     ) {
         List<ChallengeResponse> response = challengeService.findByStatus(userId, status);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
 
