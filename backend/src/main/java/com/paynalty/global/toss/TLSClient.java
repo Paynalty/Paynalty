ea4c1f2c2a56b06ce13d812aa5fb52f0d9591710
devYuraKim <dev.yurakim@gmail.com>
2025-12-19 09:53:54 +0900

    enable basic mTLS connection

---
+++ backend/src/main/java/com/paynalty/global/toss/TLSClient.java
@@ -0,0 +1,100 @@
+package com.paynalty.global.toss;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.io.*;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+public class TLSClient {
+
+    public static SSLContext createSSLContext(String certPath, String keyPath) throws Exception {
+        X509Certificate cert = loadCertificate(certPath);
+        PrivateKey key = loadPrivateKey(keyPath);
+
+        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keyStore.load(null, null);
+
+        keyStore.setCertificateEntry("client-cert", cert);
+        keyStore.setKeyEntry(
+                "client-key",
+                key,
+                new char[0],
+                new java.security.cert.Certificate[]{cert}
+        );
+
+        KeyManagerFactory kmf =
+                KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(keyStore, new char[0]);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), null, null);
+
+        return sslContext;
+    }
+
+    private static X509Certificate loadCertificate(String path) throws Exception {
+        String content = readFile(path)
+                .replace("-----BEGIN CERTIFICATE-----", "")
+                .replace("-----END CERTIFICATE-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(content);
+
+        return (X509Certificate)
+                CertificateFactory.getInstance("X.509")
+                        .generateCertificate(new ByteArrayInputStream(decoded));
+    }
+
+    private static PrivateKey loadPrivateKey(String path) throws Exception {
+        String content = readFile(path)
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(content);
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+
+        return KeyFactory.getInstance("RSA").generatePrivate(spec);
+    }
+
+    private static String readFile(String path) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        }
+    }
+
+    public static String makeRequest(String url, SSLContext sslContext) throws Exception {
+        HttpsURLConnection conn = (HttpsURLConnection) new URL(url).openConnection();
+        conn.setSSLSocketFactory(sslContext.getSocketFactory());
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(5000);
+        conn.setReadTimeout(5000);
+
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            return response.toString();
+        } finally {
+            conn.disconnect();
+        }
+    }
+
+}
