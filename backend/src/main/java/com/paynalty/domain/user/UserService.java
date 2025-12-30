c4f1db55f9306843b1939eacca29481e525e963f
kablt--6362 <wogus6362@gmail.com>
2025-12-30 17:44:10 +0900

    feat : keyword가 User의 이름or이메일에 포함된 모든 User불러오기

--- backend/src/main/java/com/paynalty/domain/user/UserService.java
+++ backend/src/main/java/com/paynalty/domain/user/UserService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -28,6 +30,12 @@ public class UserService {
     public User findByNameAndPhoneNum(String name, String phoneNum){
         User user = userRepository.findByNameAndPhoneNum(name,phoneNum).orElseThrow();
         return user;
+    }
+
+    // String 이 email 또는 name 에 포함된 모든 유저 찾기
+    public List<UserResponse> findByNameAndEmail(String identify){
+        List<User> users = userRepository.findByEmailOrName(identify);
+        return users.stream().map(UserResponse::from).toList();
     }
 
 }
