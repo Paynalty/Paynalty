c5a7a40c37591616d4dae8a4381bd74d6b79706b
mijeong09 <me9809180@gmail.com>
2025-12-31 14:30:46 +0900

    :sparkles: feat(config): mTLS WebClient Bean ('tossWebClient') 추가

--- backend/src/main/java/com/paynalty/global/config/TossApiConfig.java
+++ backend/src/main/java/com/paynalty/global/config/TossApiConfig.java
@@ -1,14 +1,20 @@
 package com.paynalty.global.config;
 
 import com.paynalty.global.toss.TLSClient;
+import io.netty.handler.ssl.SslContextBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
-import javax.net.ssl.SSLContext;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
 
 @Slf4j
 @Configuration
@@ -21,14 +27,34 @@ public class TossApiConfig {
     }
 
     @Bean
-    public SSLContext tossSslContext(
+    public io.netty.handler.ssl.SslContext tossSslContext(
             @Value("${toss.api.mtls.client-cert-path}") String certPath,
             @Value("${toss.api.mtls.client-key-path}") String keyPath
     ) throws Exception {
         Resource certResource = resourceLoader.getResource(certPath);
         Resource keyResource = resourceLoader.getResource(keyPath);
 
-        return TLSClient.createSSLContext(certResource, keyResource);
+        X509Certificate certificate = TLSClient.loadCertificate(certResource);
+        PrivateKey privateKey = TLSClient.loadPrivateKey(keyResource);
+
+        return SslContextBuilder.forClient()
+                .keyManager(privateKey, certificate)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("tossWebClient")
+    public WebClient tossWebClient(
+            io.netty.handler.ssl.SslContext tossSslContext,
+            @Value("${toss.api.base-url}") String baseUrl
+    ) {
+        HttpClient httpClient = HttpClient.create()
+                .secure(t -> t.sslContext(tossSslContext));
+
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
 
