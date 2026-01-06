f4c80651de70827868f94dc58f5105674711b379
kablt--6362 <wogus6362@gmail.com>
2026-01-06 11:22:29 +0900

    refact : 챌린지 응답 객체에 맴버 정보 포함

--- backend/src/main/java/com/paynalty/domain/challenge/Challenge.java
+++ backend/src/main/java/com/paynalty/domain/challenge/Challenge.java
@@ -79,7 +79,8 @@ public class Challenge extends BaseTimeEntity {
             , VerificationType verificationType
             , User user
             , LocalTime verifyStartAt, LocalTime verifyEndAt
-            , List<DayOfWeekType> daysOfWeek){
+            , List<DayOfWeekType> daysOfWeek
+            ){
         this.title = title;
         this.startDate = startDate;
         this.endDate = endDate;
