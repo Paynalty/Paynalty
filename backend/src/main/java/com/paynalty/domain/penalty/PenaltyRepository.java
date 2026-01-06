2211fee15bdfb7002aa339654639d7871983bbd4
kablt--6362 <wogus6362@gmail.com>
2026-01-06 17:31:35 +0900

    feat : penalty 저장소 생성

---
+++ backend/src/main/java/com/paynalty/domain/penalty/PenaltyRepository.java
@@ -0,0 +1,9 @@
+package com.paynalty.domain.penalty;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PenaltyRepository extends JpaRepository<Penalty, Long> {
+}
+
