95382cf7818c879a7ce09ce17f18af3740c5ba77
kablt--6362 <wogus6362@gmail.com>
2026-01-13 16:24:02 +0900

    refactor : 사용하지않는 에러 삭제

--- backend/src/main/java/com/paynalty/global/error/ChallengeErrorCode.java
+++ backend/src/main/java/com/paynalty/global/error/ChallengeErrorCode.java
@@ -13,7 +13,6 @@ public enum ChallengeErrorCode implements ErrorCode {
     // 챌린지 생성 시 유효성 검증 에러
     INVALID_FREQUENCY(HttpStatus.BAD_REQUEST, "CHA010", "dayOfWeeks가 없을 때는 frequency 값(양수)이 필수입니다."),
     INVALID_START_DATE_PAST(HttpStatus.BAD_REQUEST, "CHA011", "시작일은 이미 지난 날짜일 수 없습니다."),
-    INVALID_START_DATE_TODAY(HttpStatus.BAD_REQUEST, "CHA012", "시작일은 오늘 날짜일 수 없습니다. 최소 내일 이후로 설정해주세요."),
     INVALID_END_DATE_PAST(HttpStatus.BAD_REQUEST, "CHA013", "마감일은 이미 지난 날짜일 수 없습니다."),
     INVALID_END_DATE_TODAY(HttpStatus.BAD_REQUEST, "CHA014", "마감일은 오늘 날짜일 수 없습니다. 최소 내일 이후로 설정해주세요."),
     INVALID_DATE_RANGE(HttpStatus.BAD_REQUEST, "CHA015", "마감일은 시작일보다 이후여야 합니다."),
